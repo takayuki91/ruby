5e17266c4955b46b629016124ee113048dd1c9dd
EC2 Default User <ec2-user@ip-172-31-42-101.ap-northeast-1.compute.internal>
2023-04-04 06:13:43 +0000

    [Start]

---
+++ add.rb
@@ -0,0 +1,7 @@
+for i in 1..6 do
+  puts i
+end
+
+for g in { "太郎" => 185 , "二郎" => 175 } do
+  puts g
+end
---
+++ lesson6-1-2.rb
@@ -0,0 +1,11 @@
+puts "計算をはじめます"
+
+number = 2
+puts "#{number}つの値を入力してください"
+
+n_1 = gets.to_i
+n_2 = gets.to_i
+
+puts "計算結果を出力します"
+puts "#{n_1}*#{n_2}=#{n_1 * n_2}"
+puts "計算を終了します"
